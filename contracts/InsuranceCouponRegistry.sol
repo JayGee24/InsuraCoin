20c75d33ba3059232384e9602c27ae9a1f992ea0
jgenfi <jgenfi@cs.brown.edu>
2019-04-21 18:56:23 -0400

    Registry Contracts implementation

---
+++ contracts/InsuranceCouponRegistry.sol
@@ -0,0 +1,166 @@
+pragma solidity ^0.4.24;
+
+import "./Tokens/ERC721.sol";
+import "./ArbitrationContract.sol"
+
+/**
+* @title This contract inherits from ERC721 and implements and
+* contains the list of insurance coupons created by insurance
+* company
+* An insurance company can also use this contract to mint coupons
+* Provides public functions for manipulating these coupons.
+**/
+
+contract InsuranceCouponRegistry is ERC721 {
+  string public constant contractName = 'InsuranceCouponRegistry'; // For testing.
+
+  event CouponCreated(
+    address indexed owner,
+    uint256 indexed couponId
+  );
+
+  event CouponDestroyed(
+    address indexed owner,
+    uint256 indexed couponId
+  );
+
+  //Coupon struct to hold info about coupon
+  struct Coupon {
+    uint256 couponId;
+    string  name;
+    address owner;
+    address company;
+    uint256 expirationDate;
+    uint256 propertyId;
+    uint256 propertyRegistryAddress;
+    bool    invalidated;
+    string  metadata;
+  }
+
+  //ArbitrationContract address
+  address public _ab
+
+  //Array of all coupons
+  Coupon[] internal _coupons;
+
+  //List of registered insurance companies
+  mapping(address => bool) registedInsurers;
+
+  constructor(
+    address _arbitrationContract,
+  )
+  public {
+    _ab = _arbitrationContract;
+  }
+
+
+  modifier onlyInsuranceCompany(address companyId) {
+    require(registedInsurers[companyId] == true, "msg.sender is an insurance company.");*/
+    _;
+  }
+
+  modifier onlyArbitrationContract() {
+    require(msg.sender == _ab, "msg.sender is an arbitration contract");
+    _;
+  }
+
+  /**
+  * @dev Registers company as an insurer with this registry
+  * @param insurer address
+  **/
+  function registerInsurer(address insurer) public{
+    //Deliberately skip practical validation that insurance company
+    //physically/legally exists.
+    registeredInsurers[insurer] = true;
+  }
+
+  /**
+  * @dev Creates a new coupon after checking ownership and checking a valid
+  * @dev Add expiration date
+  * @param _couponId uint256 represents a specific coupon
+  * @param _uri string containing metadata/uri
+  */
+  function createInsuranceCoupon(string _metadata,
+    uint256 _daysAfter, address _couponOwner, uint256 _propertyId,
+    uint256 _propertyRegistryAddress
+
+    ) public onlyInsuranceCompany(msg.sender) returns(bool){
+    uint256 newCouponId  = _coupons.length;
+    Coupon memory newCoupon;
+    newCoupon.owner   = _couponOwner;
+    newCoupon.company = msg.sender;
+    newCoupon.expirationDate = now + _daysAfter * 1 days;
+    newCoupon.couponID   = newCouponId;
+    newCoupon.metadata   = metadata;
+    newCoupon.propertyId = _propertyId;
+    newCoupon.invalidated = false;
+    newCoupon.propertyRegistryAddress = _propertyRegistryAddress;
+
+    _coupons.push(newCoupon);
+
+    _mint(msg.sender, newCouponId);
+
+    emit CouponCreated(msg.sender,newCouponId);
+    return true;
+  }
+
+  /**
+  * @dev Transfers coupon ownership from insurer to ArbitrationContract
+  * @param _couponId uint256
+  **/
+  function transferOwnershipToArbitrationContract(uint256 _couponId)
+   public onlyInsuranceCompany(msg.sender){
+    require( ownerOf(_couponId) == msg.sender);
+    transferFrom(msg.sender,_ab,couponId);
+  }
+
+  /**
+  * @dev Transfers coupon ownership from ArbitrationContract to client
+  * @param client address
+  * @param _couponId uint256
+  * @return
+  **/
+  function transferOwnershipToClient(address client,
+     uint256 _couponId) public onlyArbitrationContract{
+    require(ownerOf(_couponId) == _ab);
+    transferFrom(_ab,client,couponId);
+  }
+
+  /**
+  * @dev Destroy Coupon
+  * @param _couponId uint256
+  **/
+  function burnCoupon(uint256 _couponId) public{
+    address couponOwner = ownerOf(_couponId);
+    require(msg.sender == couponOwner);
+    _burn(couponOwner,_couponId);
+  }
+
+  /**
+  * @dev Check if coupon is valid - still permitted for use or unexpired
+  * @param _couponId uint256
+  * @return isValid bool
+  **/
+  function isCouponValid(uint256 _couponId) public
+  returns(bool){
+    if (!exists(_couponId)){
+      return false;
+    }
+    Coupon coupon = _coupons[_couponId];
+    if (coupon.invalidated) {
+      return false
+    }
+    if (now > coupon.expirationDate){
+      _coupons[_couponId].invalidated = true;
+      return false;
+    }
+  }
+
+  /**
+  * @dev Event is triggered if property token is registered
+  * @param _by address of the registrar
+  * @param _tokenId uint256 represents a specific propety token
+  */
+  event CouponCreated(address _by, uint256 _couponId);
+
+}
--- contracts/PropertyRegistry.sol
+++ contracts/PropertyRegistry.sol
@@ -1,36 +1,64 @@
 pragma solidity ^0.4.24;
 
-import "./Tokens/ERC721.sol";
+import "./Tokens/ERC721Token.sol";
 
 /**
- * This contract inherits from ERC721 and implements and
- * contains the list of property-items registered by clients.
- */
+* @title This contract inherits from ERC721 and implements and
+* contains the list of property-items registered by clients.
+* Acts as a contract that mints, stores, transfers
+* and burns property tokens.
+* Provides functions for manipulating property tokens.
+**/
 
-contract PropertyRegistry is ERC721 {
+contract PropertyRegistry is ERC721Token {
   string public constant contractName = 'PropertyRegistry'; // For testing.
 
-  event PropertyRegistered(
-    address indexed owner,
-    uint256 indexed tokenId
-  );
-  /**
-  *
-  **/
-
-  function PropertyRegistry(string _name, string _symbol)
+  constructor(string _name, string _symbol)
     public ERC721Token(_name, _symbol) {}
 
+  /**
+  * @dev Public function to register a new property token
+  * @dev Call the ERC721Token minter
+  * @param _tokenId uint256 represents a specific deed
+  * @param _uri string containing metadata/uri
+  */
   function registerProperty(uint256 _tokenId, string _uri) public {
     _mint(msg.sender, _tokenId);
     addPropertyMetadata(_tokenId, _uri);
-    //TODO: Emit Property registered event
     emit PropertyRegistered(msg.sender,_tokenId);
   }
 
-  function addPropertyMetadata(uint256 _tokenId, string _uri) public returns(bool){
+  /**
+  * @dev Public function to add metadata to a property token
+  * @param _tokenId represents a specific property token
+  * @param _uri text which describes the characteristics of a given property
+  *        token
+  * @return whether the property token metadata was added to the registry
+  */
+  function addPropertyMetadata(uint256 _tokenId, string _uri) public
+  returns(bool){
     _setTokenURI(_tokenId, _uri);
     return true;
   }
+
+  /**
+  * @dev Public function to verify that property belongs to a client
+  * @param client uint256 client in question
+  * @param _token uint256  propery being checked against client.
+  * @return: bool for whether client owns token
+  **/
+  function verifyPropertyOwnership(uint256 client, uint256 _tokenId) public
+  returns(bool){
+    uint256 tokenIndex = ownedTokensIndex[_tokenId];
+    uint256 trackedClientToken =  ownedTokens[client][tokenIndex]
+    return trackedClientToken == _tokenId;
+  }
+
+  /**
+  * @dev Event is triggered if property token is registered
+  * @param _by address of the registrar
+  * @param _tokenId uint256 represents a specific propety token
+  */
+  event ProperyRegistered(address _by, uint256 _tokenId);
 
 }
